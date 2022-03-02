54ea5bd751ba89027c1ba2db271f1e8781043b14
ink19 <ink19@qq.com>
2022-03-02 15:19:51 +0800

    anim over

--- src/components/GridMap.vue
+++ src/components/GridMap.vue
@@ -1,169 +1,38 @@
 <template>
-  <div ref="gridmap" class="map"></div>
+  <div class="map" ref="gridmapcontainer">
+    <canvas ref="gridmap"></canvas>
+  </div>
 </template>
 
 <script lang="ts">
+import { GridDraw } from '@/grid/grid'
 import { defineComponent } from 'vue'
-import * as echarts from 'echarts'
-import 'echarts/lib/component/grid'
 
-const gridWidth = 10
-const gridHeight = 10
-const agentNumber = 20
-const rewardNumber = 20
+let gridDraw: null | GridDraw = null
 
-const priodSecond = 5
-
-let gridEchart : null | echarts.ECharts = null
-
-interface PointInterface {
-  name: string,
-  value: number[]
-}
-
-interface EdgeInterface {
-  source: string,
-  target: string
-}
-
-interface AgentInterface {
-  name: string,
-  index: number,
-  path: number[]
-}
-
-interface AgentPathInterface {
-  coords: number[][],
-  effect: {
-    period: number
-  }
-}
+const priorTime = 4
 
 export default defineComponent({
   name: 'GridMap',
   data () {
-    return {
-      chartOptions: {
-        title: {
-          text: 'Traffic Map'
-        },
-        xAxis: {
-          show: false,
-          type: 'value',
-          min: 'dataMin',
-          max: 'dataMax'
-        },
-        yAxis: {
-          show: false,
-          type: 'value',
-          min: 'dataMin',
-          max: 'dataMax'
-        },
-        series: [{
-          type: 'graph',
-          layout: 'none',
-          coordinateSystem: 'cartesian2d',
-          symboSize: 20,
-          roam: true,
-          edgeSymbolSize: 4,
-          data: [] as any,
-          links: [] as any
-        }, {
-          name: 'Route',
-          type: 'lines',
-          coordinateSystem: 'cartesian2d',
-          polyline: true,
-          lineStyle: {
-            width: 0
-          },
-          effect: {
-            show: true,
-            symbolSize: 20,
-            symbol: 'circle',
-            color: '#ff0000',
-            trailLength: 0,
-            loop: true
-          },
-          animation: false,
-          data: [] as any
-        }]
-      },
-      agents: [] as AgentInterface[]
-    }
+    return {}
   },
   mounted () {
-    const chartDom = this.$refs.gridmap as HTMLElement
-    for (let loopHeight = 0; loopHeight < gridHeight; ++loopHeight) {
-      for (let loopWidth = 0; loopWidth < gridWidth; ++loopWidth) {
-        const node = {
-          name: (loopHeight * gridWidth + loopWidth).toString(),
-          value: [loopWidth * 50 + 50, loopHeight * 50 + 50]
-        }
-        this.chartOptions.series[0].data.push(node)
-        if (loopHeight !== 0) {
-          const edge = {
-            source: ((loopHeight - 1) * gridWidth + loopWidth).toString(),
-            target: ((loopHeight) * gridWidth + loopWidth).toString()
-          }
-          this.chartOptions.series[0].links.push(edge)
-        }
-        if (loopWidth !== 0) {
-          const edge = {
-            source: ((loopHeight) * gridWidth + loopWidth - 1).toString(),
-            target: ((loopHeight) * gridWidth + loopWidth).toString()
-          }
-          this.chartOptions.series[0].links.push(edge)
-        }
-      }
-    }
-    for (let loopAgent = 0; loopAgent < agentNumber; ++loopAgent) {
-      const randomPosition = Math.floor(Math.random() * gridWidth * gridHeight)
-      const agentItem = {
-        name: loopAgent.toString(),
-        index: loopAgent,
-        path: [randomPosition, randomPosition]
-      } as AgentInterface
-      this.agents.push(agentItem)
-      this.chartOptions.series[1].data.push({
-        coords: [
-          [this.chartOptions.series[0].data[randomPosition].value[0], this.chartOptions.series[0].data[randomPosition].value[1]],
-          [this.chartOptions.series[0].data[randomPosition].value[0], this.chartOptions.series[0].data[randomPosition].value[1]]
-        ],
-        effect: {
-          period: priodSecond
-        }
-      } as AgentPathInterface)
-    }
-    gridEchart = echarts.init(chartDom)
-    if (gridEchart !== null) {
-      gridEchart.setOption(this.chartOptions)
-    }
-    setInterval(this.run, priodSecond * 1000)
+    const drawCanvasContainer = this.$refs.gridmapcontainer as HTMLElement
+    const drawCanvas = this.$refs.gridmap as HTMLCanvasElement
+    drawCanvas.height = drawCanvasContainer.clientHeight
+    drawCanvas.width = drawCanvasContainer.clientWidth
+    console.log(drawCanvas.height, drawCanvas.width)
+    gridDraw = new GridDraw(drawCanvas)
+    gridDraw.setEnv(10, 10)
+    gridDraw.setAgentNumber(20)
+    gridDraw.render()
+    setInterval(this.run, priorTime * 1000)
   },
   methods: {
     run () {
-      this.agents.forEach((value) => {
-        const choosePoint = []
-        const nowPosition = value.path[value.path.length - 1]
-        if (nowPosition / gridWidth >= 1) {
-          choosePoint.push(nowPosition - gridWidth)
-        }
-        if (nowPosition / gridWidth < gridHeight - 1) {
-          choosePoint.push(nowPosition + gridWidth)
-        }
-        if (nowPosition % gridHeight !== 0) {
-          choosePoint.push(nowPosition - 1)
-        }
-        if (nowPosition % gridHeight !== gridWidth - 1) {
-          choosePoint.push(nowPosition + 1)
-        }
-        const nextPosition = choosePoint[Math.floor(Math.random() * choosePoint.length)]
-        value.path.push(nextPosition)
-        this.chartOptions.series[1].data[value.index].coords.push([this.chartOptions.series[0].data[nextPosition].value[0], this.chartOptions.series[0].data[nextPosition].value[1]])
-        this.chartOptions.series[1].data[value.index].effect.period += priodSecond
-      })
-      if (gridEchart !== null) {
-        gridEchart.setOption(this.chartOptions)
+      if (gridDraw !== null) {
+        gridDraw.setAgentPath()
       }
     }
   }
--- src/grid/grid.ts
+++ src/grid/grid.ts
@@ -1,32 +1,207 @@
 import { fabric } from 'fabric'
 
+interface PositionInterface {
+  top: number,
+  left: number
+}
+
+interface NodeInterface {
+  position: PositionInterface,
+  size: number,
+  obj: fabric.Circle,
+}
+
+interface AgentInterface {
+  nowPosition: PositionInterface,
+  speed: PositionInterface,
+  actionLeftFrame: number,
+  nowIndex: number,
+  path: number[],
+  obj: fabric.Circle
+}
 export class GridDraw {
   private canvas : fabric.Canvas
-  private height : number = 0
-  private width : number = 0
-  public constructor(dom: HTMLCanvasElement) {
+  private height = 0
+  private width = 0
+  private edgeWidth = 2
+  private nodeMaxSize = 10
+  private edgeColor = '#bbb'
+  private nodeColor = '#5470c6'
+  private agentColor = '#73c0de'
+  private agentSize = 10
+  private nodes: NodeInterface[] = []
+  private agents: AgentInterface[] = []
+  private minWaitTime = 300 // ms
+  private priorTime = 3000 // ms
+  private fps = 30
+
+  public constructor (dom: HTMLCanvasElement) {
     this.canvas = new fabric.Canvas(dom)
+    this.canvas.selection = false
   }
-  
-  public setEnv(height: number, width: number) {
+
+  public setEnv (height: number, width: number) : void {
     this.height = height
     this.width = width
     this.draw()
   }
 
-  private draw() {
+  private draw () : void {
     this.canvas.clear()
+    const canvasHeight = this.canvas.height as number
+    const canvasWidth = this.canvas.width as number
+    const heightInterval = Math.floor(canvasHeight / this.height)
+    const widthInterval = Math.floor(canvasWidth / this.width)
+    const intervalPx = Math.min(heightInterval, widthInterval)
+    const heightBf = (canvasHeight - (intervalPx * (this.height - 1)) - 2 * this.nodeMaxSize) / 2
+    const widthBf = (canvasWidth - (intervalPx * (this.width - 1)) - 2 * this.nodeMaxSize) / 2
+    // edge
     for (let loopHeight = 0; loopHeight < this.height; ++loopHeight) {
       for (let loopWidth = 0; loopWidth < this.width; ++loopWidth) {
-        let node = new fabric.Circle({
-          left: 50 + 50 * loopWidth,
-          top: 50 + 50 * loopHeight,
-          fill: 'blue',
-          height: 10,
-          width: 10
+        if (loopHeight !== 0) {
+          const edge = new fabric.Line([
+            widthBf + intervalPx * loopWidth + this.nodeMaxSize - this.edgeWidth / 2, heightBf + intervalPx * loopHeight + this.nodeMaxSize,
+            widthBf + intervalPx * loopWidth + this.nodeMaxSize - this.edgeWidth / 2, heightBf + intervalPx * (loopHeight - 1) + this.nodeMaxSize
+          ], {
+            fill: this.edgeColor,
+            stroke: this.edgeColor,
+            strokeWidth: this.edgeWidth,
+            selectable: false,
+            evented: false
+          })
+          this.canvas.add(edge)
+        }
+
+        if (loopWidth !== 0) {
+          const edge = new fabric.Line([
+            widthBf + intervalPx * loopWidth + this.nodeMaxSize, heightBf + intervalPx * loopHeight + this.nodeMaxSize - this.edgeWidth / 2,
+            widthBf + intervalPx * (loopWidth - 1) + this.nodeMaxSize, heightBf + intervalPx * (loopHeight) + this.nodeMaxSize - this.edgeWidth / 2
+          ], {
+            fill: this.edgeColor,
+            stroke: this.edgeColor,
+            strokeWidth: this.edgeWidth,
+            selectable: false,
+            evented: false
+          })
+          this.canvas.add(edge)
+        }
+      }
+    }
+
+    // node
+    for (let loopHeight = 0; loopHeight < this.height; ++loopHeight) {
+      for (let loopWidth = 0; loopWidth < this.width; ++loopWidth) {
+        const tempNode = {
+          position: {
+            left: widthBf + intervalPx * loopWidth,
+            top: heightBf + intervalPx * loopHeight
+          },
+          size: 2 + Math.random() * 8,
+          obj: null as unknown as fabric.Circle
+        } as NodeInterface
+        const node = new fabric.Circle({
+          left: tempNode.position.left + this.nodeMaxSize - tempNode.size,
+          top: tempNode.position.top + this.nodeMaxSize - tempNode.size,
+          fill: this.nodeColor,
+          radius: tempNode.size,
+          selectable: false,
+          evented: false
         })
+
+        tempNode.obj = node
+        this.nodes.push(tempNode)
         this.canvas.add(node)
       }
     }
+  }
+
+  public setAgentNumber (agentNumber : number) : void {
+    for (let loopAgent = 0; loopAgent < agentNumber; ++loopAgent) {
+      const tempNodeIndex = Math.floor(this.nodes.length * Math.random())
+      const tempAgent = {
+        nowIndex: 0,
+        path: [tempNodeIndex, tempNodeIndex],
+        obj: null as unknown as fabric.Circle,
+        nowPosition: {
+          top: this.nodes[tempNodeIndex].position.top + this.nodeMaxSize - this.agentSize,
+          left: this.nodes[tempNodeIndex].position.left + this.nodeMaxSize - this.agentSize
+        },
+        speed: {
+          top: 0,
+          left: 0
+        },
+        actionLeftFrame: 0
+      } as AgentInterface
+
+      const agentObj = new fabric.Circle({
+        left: tempAgent.nowPosition.left,
+        top: tempAgent.nowPosition.top,
+        fill: this.agentColor,
+        radius: this.agentSize,
+        selectable: false,
+        evented: false
+      })
+
+      tempAgent.obj = agentObj
+      this.agents.push(tempAgent)
+      this.canvas.add(agentObj)
+    }
+  }
+
+  public run () : void {
+    this.agents.forEach((value, index) => {
+      if (value.actionLeftFrame <= 0) {
+        if (value.nowIndex === value.path.length - 1) {
+          value.actionLeftFrame = Math.floor(this.minWaitTime / 1000 * this.fps)
+          value.speed = {
+            top: 0,
+            left: 0
+          }
+          return
+        }
+        const nextPosition = this.nodes[value.path[value.nowIndex + 1]].position
+        const countFrame = Math.floor(this.priorTime / 1000 * this.fps)
+        value.actionLeftFrame = countFrame
+        value.speed = {
+          top: (nextPosition.top - value.nowPosition.top) / countFrame,
+          left: (nextPosition.left - value.nowPosition.left) / countFrame
+        }
+        value.nowIndex++
+      }
+      value.actionLeftFrame--
+      value.nowPosition.left += value.speed.left
+      value.nowPosition.top += value.speed.top
+      value.obj.set('top', value.nowPosition.top)
+      value.obj.set('left', value.nowPosition.left)
+      value.obj.setCoords()
+    })
+  }
+
+  public render () : void {
+    setInterval(() => {
+      this.run()
+      this.canvas.renderAll()
+    }, 1000 / this.fps)
+  }
+
+  public setAgentPath () : void {
+    this.agents.forEach((value) => {
+      const lastPos = value.path[value.path.length - 1]
+      const choicePos : number[] = []
+      if (lastPos / this.width >= 1) {
+        choicePos.push(lastPos - this.width)
+      }
+      if (lastPos / this.width < this.height - 1) {
+        choicePos.push(lastPos + this.width)
+      }
+      if (lastPos % this.width !== 0) {
+        choicePos.push(lastPos - 1)
+      }
+      if (lastPos % this.width !== this.width - 1) {
+        choicePos.push(lastPos + 1)
+      }
+      const nextPos = choicePos[Math.floor(Math.random() * choicePos.length)]
+      value.path.push(nextPos)
+    })
   }
 }
